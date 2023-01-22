77de66c4c7a794fae6a53dce9807f451abb1b74a
Maarten Lauwaert <github@maartenlauwaert.eu>
2023-01-22 18:55:27 +0100

    last before demo

--- components/ActionCard.vue
+++ components/ActionCard.vue
@@ -2,9 +2,9 @@
   <div class="overflow-hidden bg-white shadow sm:rounded-lg relative">
     <div class="flex justify-between">
       <div class="px-4 py-5 sm:px-6 ">
-        <div class="flex ">
-          <StarIcon v-if="action.steps[0].milestone" class="pt-1 mr-2 h-5 w-5 fill-cyan-500" aria-hidden="true" />
-          <h3 class="text-lg pb-1 font-medium leading-6 text-gray-900">{{ action.description }}</h3>
+        <div class="flex flex-col">
+          <StarIcon v-if="action.steps[0].milestone" class="pt-1 mb-4 mr-2 h-5 w-5 fill-cyan-500" aria-hidden="true" />
+          <h3 class="text-md pb-1 font-medium leading-6 text-gray-900">{{ action.description }}</h3>
         </div>
         <div class="flex flex-row gap-2 pt-2 pb-4">
           <div class="flex items-center">
